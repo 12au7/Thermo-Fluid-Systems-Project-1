e35c34ab565d1d0f91714f2c481d732b170f167e
David Notaro <starwarsdave@gmail.com>
2018-02-26 13:13:53 -0500

    Fixed magnitude of values and added F and H plants

--- Project1.m
+++ Project1.m
@@ -2,7 +2,7 @@
 clear
 c = 1722; % [MW] E plant capacity
 c_f = .53; % Capacity factor
-PPA = .09; % [$/KWH] Power Purchase Agreement
+PPA = .51; % [$/KWH] Power Purchase Agreement
 eta = .53; % Thermal efficiency
 fc = 8.71; % [$/Million BTU] fuel cost
 c02e = 117; % [lbs c02/Million BTU] c02 emmisions
@@ -17,42 +17,154 @@ cof = (qinc/1000000) * fc; % [$] cost of fuel per year
 c02 = (qinc/1000000) * c02e; % [lbs] of c02 per year
 cfc = (cf/2000) * c02; % [$] carbon fee per year
 
-net_e = e_py - cof; % Net earned per year (no carbon fee)
-net_e_cf = net_e - cfc; % Net earned per year (with carbon fee)
-
-CF = [-.3*cc;-.3*cc;-.2*cc;-.3*cc;net_e.*ones(25,1)];
-i = [0:28]';
-PV = CF./(1+.07).^i;
-
-NPV_NoCarbon = (sum(PV)/1000000); %[M$] Net Present Value (No C02 tax)
+net_e = (e_py - cof)/1000000; % [M$] Net earned per year (no carbon fee)
+net_e_cf = net_e - (cfc/1000000); % [M$] Net earned per year (with carbon fee)
 
 CF_Carbon = [-.3*cc;-.3*cc;-.2*cc;-.3*cc;net_e_cf.*ones(25,1)];
-PV_C = CF_Carbon./(1+.07).^i;
+i = [0:28]';
+PV_C = CF_Carbon./(1+.1).^i; % Present Value Assuming 10% interest rate
 
-NPV_Carbon = (sum(PV_C)/1000000); %[M$] Net Present Value (w/C02 tax)
+NPV_Carbon = sum(PV_C); %[M$] Net Present Value (w/C02 tax)
 
-Cum_cash(1)=CF_Carbon(1);
+Cum_cash(1)=CF_Carbon(1); %Cumulative Cash w/ carbon fee
 	for k=2:29;
     	Cum_cash(k)=Cum_cash(k-1)+CF_Carbon(k);
     end
 
-
+% With Carbon Fee
 x_pts = 0:28;  %x values
 y_pts = Cum_cash; %y values
-f = @(x)interp1(x_pts,y_pts,x,'linear'); 
+fc = @(x)interp1(x_pts,y_pts,x,'linear'); 
 plot(x_pts,y_pts)
+title('Cash Flow diagram for Plant E (w/Carbon Fee)')
+xlabel('Years')
+ylabel('Millions of Dollars')
 %find where the interpolation yields zero.
-[year,prof]=fzero(f,10); % 5 is a guess
+[year,prof] = fzero(fc,5); % 5 is a guess
 PayBackPeriod = year
 %ROI=Average Profit/ Total Investment
 %C=[-10000,-5000,7000,7000,15000];
-Av_prof= sum(CF_Carbon)/28;
-Inv= (cc);
+Av_prof = sum(CF_Carbon)/28;
+Inv = (cc);
 
 ROI=Av_prof/Inv; %
 ROI_CCGT = ROI*100
 
 [xval,fval] = fzero(@(x)(CF_Carbon'*(1+x).^-(0:length(CF_Carbon')-1).'), 3);
 
-IRR=xval;
-IRR_CCGT = IRR*100
+IRR = xval;
+IRR_CCGT = IRR*100 % Percent
+
+% F Plant
+
+c = 1722; % [MW] E plant capacity
+c_f = .53; % Capacity factor
+PPA = .51; % [$/KWH] Power Purchase Agreement
+eta = .53; % Thermal efficiency
+fc = 8.71; % [$/Million BTU] fuel cost
+c02e = 117; % [lbs c02/Million BTU] c02 emmisions
+cf = 60; % [$/ton c02] carbon fee
+cc = 2152.5;  % [M$] construction cost
+
+e_py = c * c_f * 365 * 24 * PPA * 1000; % [$] earned per year
+
+qinc = c * c_f * 81891387.76 * 365 /eta; % BTUs produced in 1 year
+
+cof = (qinc/1000000) * fc; % [$] cost of fuel per year
+c02 = (qinc/1000000) * c02e; % [lbs] of c02 per year
+cfc = (cf/2000) * c02; % [$] carbon fee per year
+
+net_e = (e_py - cof)/1000000; % [M$] Net earned per year (no carbon fee)
+net_e_cf = net_e - (cfc/1000000); % [M$] Net earned per year (with carbon fee)
+
+CF_Carbon = [-.3*cc;-.3*cc;-.2*cc;-.3*cc;net_e_cf.*ones(25,1)];
+i = [0:28]';
+PV_C = CF_Carbon./(1+.1).^i; % Present Value Assuming 10% interest rate
+
+NPV_Carbon = sum(PV_C); %[M$] Net Present Value (w/C02 tax)
+
+Cum_cash(1)=CF_Carbon(1); %Cumulative Cash w/ carbon fee
+	for k=2:29;
+    	Cum_cash(k)=Cum_cash(k-1)+CF_Carbon(k);
+    end
+
+% With Carbon Fee
+x_pts = 0:28;  %x values
+y_pts = Cum_cash; %y values
+fc = @(x)interp1(x_pts,y_pts,x,'linear'); 
+plot(x_pts,y_pts)
+title('Cash Flow diagram for Plant E (w/Carbon Fee)')
+xlabel('Years')
+ylabel('Millions of Dollars')
+%find where the interpolation yields zero.
+[year,prof] = fzero(fc,5); % 5 is a guess
+PayBackPeriod = year
+%ROI=Average Profit/ Total Investment
+%C=[-10000,-5000,7000,7000,15000];
+Av_prof = sum(CF_Carbon)/28;
+Inv = (cc);
+
+ROI=Av_prof/Inv; %
+ROI_CCGT = ROI*100
+
+[xval,fval] = fzero(@(x)(CF_Carbon'*(1+x).^-(0:length(CF_Carbon')-1).'), 3);
+
+IRR = xval;
+IRR_CCGT = IRR*100 % Percent
+
+% H Plant
+
+c = 1722; % [MW] E plant capacity
+c_f = .53; % Capacity factor
+PPA = .51; % [$/KWH] Power Purchase Agreement
+eta = .53; % Thermal efficiency
+fc = 8.71; % [$/Million BTU] fuel cost
+c02e = 117; % [lbs c02/Million BTU] c02 emmisions
+cf = 60; % [$/ton c02] carbon fee
+cc = 2152.5;  % [M$] construction cost
+
+e_py = c * c_f * 365 * 24 * PPA * 1000; % [$] earned per year
+
+qinc = c * c_f * 81891387.76 * 365 /eta; % BTUs produced in 1 year
+
+cof = (qinc/1000000) * fc; % [$] cost of fuel per year
+c02 = (qinc/1000000) * c02e; % [lbs] of c02 per year
+cfc = (cf/2000) * c02; % [$] carbon fee per year
+
+net_e = (e_py - cof)/1000000; % [M$] Net earned per year (no carbon fee)
+net_e_cf = net_e - (cfc/1000000); % [M$] Net earned per year (with carbon fee)
+
+CF_Carbon = [-.3*cc;-.3*cc;-.2*cc;-.3*cc;net_e_cf.*ones(25,1)];
+i = [0:28]';
+PV_C = CF_Carbon./(1+.1).^i; % Present Value Assuming 10% interest rate
+
+NPV_Carbon = sum(PV_C); %[M$] Net Present Value (w/C02 tax)
+
+Cum_cash(1)=CF_Carbon(1); %Cumulative Cash w/ carbon fee
+	for k=2:29;
+    	Cum_cash(k)=Cum_cash(k-1)+CF_Carbon(k);
+    end
+
+% With Carbon Fee
+x_pts = 0:28;  %x values
+y_pts = Cum_cash; %y values
+fc = @(x)interp1(x_pts,y_pts,x,'linear'); 
+plot(x_pts,y_pts)
+title('Cash Flow diagram for Plant E (w/Carbon Fee)')
+xlabel('Years')
+ylabel('Millions of Dollars')
+%find where the interpolation yields zero.
+[year,prof] = fzero(fc,5); % 5 is a guess
+PayBackPeriod = year
+%ROI=Average Profit/ Total Investment
+%C=[-10000,-5000,7000,7000,15000];
+Av_prof = sum(CF_Carbon)/28;
+Inv = (cc);
+
+ROI=Av_prof/Inv; %
+ROI_CCGT = ROI*100
+
+[xval,fval] = fzero(@(x)(CF_Carbon'*(1+x).^-(0:length(CF_Carbon')-1).'), 3);
+
+IRR = xval;
+IRR_CCGT = IRR*100 % Percent
